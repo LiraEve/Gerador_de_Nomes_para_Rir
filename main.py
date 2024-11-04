02ffc6f602ed4d6f0be63ff9d2192fe68e7e1cd0
Evellyn Lira <evyramoslira@gmail.com>
2024-11-04 19:00:13 -0300

    Espaçamento e comentário

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+#Abecedário
+
 alfabeto="A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z"
+alfabeto=alfabeto.replace(" ", "")
 alfabeto=alfabeto.split(",")
 
 print(alfabeto)
