b41f608d6c6135c61a77e91a28fffb566da49820
Evellyn Lira <evyramoslira@gmail.com>
2024-11-04 20:49:55 -0300

    Código ganhando forma + pequenas correções

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
 import random
 
-#Definir funções
+# Definir funções
 def sortearLetraAlfabeto():
-    LetraSorteada=alfabeto[random.randint(0, len(alfabeto)-1)]
-    print(LetraSorteada)
+    LetraSorteada = alfabeto[random.randint(0, len(alfabeto) - 1)]
+    tipo = None
+    for letra in vogais:
+        if LetraSorteada == letra:
+            tipo = "vogal"
+            break
+    for letra in consoante:
+        if LetraSorteada == letra:
+            tipo = "consoante"
+            break
+    print(f'Letra sorteada: {LetraSorteada}, tipo: {tipo}')
+    return LetraSorteada, tipo
 
-#Abecedário
+# Abecedário
+alfabeto = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z"
+alfabeto = alfabeto.replace(" ", "")
+alfabeto = alfabeto.split(",")
 
-alfabeto="A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z"
-alfabeto=alfabeto.replace(" ", "")
-alfabeto=alfabeto.split(",")
+# Vogais e Consoantes
+vogais = []
+consoante = []
+palavra = []
 
-#Vogais e Consoantes
-vogais=list()
-consoante=list()
-palavra=list()
-
-#Começando a Gerar
+# Separando vogais e consoantes
 for letra in alfabeto:
-    if letra=="A" or letra=="E" or letra=="I" or letra=="O" or letra=="U":
+    if letra in ["A", "E", "I", "O", "U"]:
         vogais.append(letra)
     else:
         consoante.append(letra)
-print(f'lista de vogais: {vogais}, tamanho: {len(vogais)}')
-print(f'lista de consoantes: {consoante}, tamanho: {len(consoante)}')
 
-#Tamanho da Palavra
-tamanho=6
+print(f'Lista de vogais: {vogais}, tamanho: {len(vogais)}')
+print(f'Lista de consoantes: {consoante}, tamanho: {len(consoante)}')
 
-#Formando Palavra
+# Tamanho da Palavra
+tamanho = 6
 
-sortearLetraAlfabeto()
-
-print(alfabeto)
+# Formando Palavra
+print(sortearLetraAlfabeto())
