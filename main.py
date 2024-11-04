bfecbdaa0dd349a997af49c2723b18e029211112
Evellyn Lira <evyramoslira@gmail.com>
2024-11-04 19:16:13 -0300

    Separando Vogais de Consoantes

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ alfabeto="A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X
 alfabeto=alfabeto.replace(" ", "")
 alfabeto=alfabeto.split(",")
 
+#Vogais e Consoantes
 vogais=list()
 consoantes=list()
 palavra=list()
+
+#Começando a Gerar
+for letra in alfabeto:
+    if letra=="A" or letra=="E" or letra=="I" or letra=="O" or letra=="U":
+        vogais.append(letra)
+    else:
+        consoantes.append(letra)
 
 print(alfabeto)
